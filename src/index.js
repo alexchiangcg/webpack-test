feff388878c365f3b1b4f9f669c8c2f821c3369b
alex <alexchiang.oa@gmail.com>
2020-03-03 20:53:19 +0800

    [modify] import js test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+import test from './test.js';
+
+window.onload=function() {
+    test();
+};
+
+
+console.log(test);
+
 const Name = ["Mike", "Jacky", "Andy", "Scars"];
 Name.forEach((obj, idx)=> console.log(`${idx} => ${obj}`));
